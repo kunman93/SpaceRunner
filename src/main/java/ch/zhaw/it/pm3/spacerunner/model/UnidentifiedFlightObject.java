953d55c188f85014a2ca4f59fb92b562829f63de
Peter Blattmann <blattpet@students.zhaw.ch>
2020-10-09 15:08:47 +0200

    Added Methods & Variables to <? extends SpaceElement> & <SpaceElement>

--- src/main/java/ch/zhaw/it/pm3/spacerunner/model/UnidentifiedFlightObject.java
+++ src/main/java/ch/zhaw/it/pm3/spacerunner/model/UnidentifiedFlightObject.java
@@ -1,4 +1,9 @@
 package ch.zhaw.it.pm3.spacerunner.model;
 
+import java.awt.*;
+
 public class UnidentifiedFlightObject extends Obstacle {
+    public UnidentifiedFlightObject(Point startPosition, int width, int length) {
+        super(startPosition, width, length);
+    }
 }
