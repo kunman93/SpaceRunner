623e022b6a5b340af2b068d134d5e77dd03609c9
Michael Isler <islermic@students.zhaw.ch>
2020-10-19 16:47:11 +0200

    Deleted Game and moved code into Gamecotroller

--- src/main/java/ch/zhaw/it/pm3/spacerunner/controller/GameController.java
+++ src/main/java/ch/zhaw/it/pm3/spacerunner/controller/GameController.java
@@ -1,13 +1,28 @@
 package ch.zhaw.it.pm3.spacerunner.controller;
 
-import ch.zhaw.it.pm3.spacerunner.model.Game;
+import ch.zhaw.it.pm3.spacerunner.model.element.*;
+import ch.zhaw.it.pm3.spacerunner.model.gamedata.PlayerProfile;
+import ch.zhaw.it.pm3.spacerunner.util.FileUtil;
+import javafx.scene.input.KeyCode;
+
+import java.awt.*;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class GameController {
 
     private boolean isRunning = false;
     private boolean isPaused = false;
 
-    private Game game;
+    private int collectedCoins;
+    private int distance;
+    private int points;
+
+    private SpaceShip spaceShip;
+    private Set<SpaceElement> elementMap;
+    private PlayerProfile playerProfile;
+
     private GameView gameView;
 
     public void setView(GameView gameView){
@@ -16,27 +31,29 @@ public class GameController {
 
 
     public void startGame() throws Exception {
-        game = new Game();
-
         if(gameView == null){
             //TODO: Make own exception types and handle
             throw new Exception();
         }
 
         isRunning = true;
-        while(isRunning && isPaused == false){
+        while(isRunning){
+
+            if(isPaused){
+
+            }
 
             //TODO: Get KeyEvent and
             boolean keypressed = false;
             if(keypressed){
-                game.moveSpaceShip(null);
+                moveSpaceShip(null);
             }
             //TODO: Draw Elements to canvas
-            gameView.drawSpaceElements(game.getDrawables());
-            game.collisonDetector();
+            gameView.drawSpaceElements(getDrawables());
+            collisionDetector();
 
-            game.generateObstacles();
-            game.moveElements();
+            generateObstacles();
+            moveElements();
         }
 
         //TODO: Add collected coins to playerProfile and save it!
@@ -51,6 +68,95 @@ public class GameController {
         isPaused = !isPaused;
     }
 
+    /**
+     * adds obstacles, powerups and coins to map
+     * */
+    private void initializeObstacles() {
+        playerProfile = FileUtil.loadProfile();
+        try {
+            //TODO: SetVisuals for Coins, UFO, Powerups etc.
+            SpaceShip.setVisual(FileUtil.loadImage(playerProfile.getPlayerImageId() + ".svg"));
+            spaceShip = new SpaceShip(new Point(20, 100), 50, 200);
+        } catch (IOException e) {
+            //TODO: Handle: Should never happen unless theres a model which doesnt have an corresponding image in resources
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        distance = 0;
+        collectedCoins = 0;
+        elementMap = new HashSet<>();
+    }
+
+    public Set<SpaceElement> getDrawables(){
+        Set<SpaceElement> spaceElements = new HashSet<>(elementMap);
+        spaceElements.add(spaceShip);
+
+        return spaceElements;
+    }
+
+
+
+    /**
+     * TODO: Move or delete
+     * saves collected coins as well as the distance (if it is a new record) to a local file
+     *
+     private void saveState() {}
+     */
+
+    /**
+     * - starts game, if this is the first movement
+     * --> transmit new position to spaceship
+     * */
+    public void moveSpaceShip(KeyCode input) {
+        switch (input) {
+            case UP:
+                //spaceShip.move(1);
+                break;
+            case DOWN:
+                //spaceShip.move(-1);
+        }
+        System.out.println(input.toString());
+    }
+
+
+    public void generateObstacles(){
+
+        //TODO: This is not how it should be => Generate from presets and only randomly
+        try {
+            elementMap.add(new Coin(new Point(20, 100), 20, 20));
+            elementMap.add(new UnidentifiedFlightObject(new Point(20, 100), 20, 20));
+            elementMap.add(new PowerUp(new Point(20, 100), 20, 20));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void moveElements(){
+        //TODO: move all movable elements
+        //TODO: delete alls with x < 0
+    }
+
+    /**
+     * checks continuously for a crash with an SpaceElement
+     * */
+    public boolean collisionDetector() {
+
+
+        //TODO: Loop over all elements and check for collision
+            /*SpaceElement spaceElement = elementMap.getOrDefault(spaceShip.getPosition(), null);
+
+            if (spaceElement instanceof Coins) coinCollected();
+            if (spaceElement instanceof PowerUp) break; //spaceShip.setPowerUp()
+            if (spaceElement instanceof Obstacle) ended = true;*/
+        return false;
+    }
+
+    private void coinCollected () {
+        collectedCoins++;
+    }
 
 
 }
