f9b7e484b93c0c73a43ad5272f8804a99792e42b
Michael Isler <islermic@students.zhaw.ch>
2020-10-25 17:05:51 +0100

    Implemented new tests for persitence util and added generic methods

---
+++ src/test/java/ch/zhaw/it/pm3/spacerunner/technicalservices/persistence/TestDataModelExtended.java
@@ -0,0 +1,33 @@
+package ch.zhaw.it.pm3.spacerunner.technicalservices.persistence;
+
+public class TestDataModelExtended extends TestDataModel {
+
+    private String extendedString;
+    private int extendedInt;
+
+    public TestDataModelExtended(int testNumber, String extendedString, int extendedInt) {
+        super(testNumber);
+        this.extendedString = extendedString;
+        this.extendedInt = extendedInt;
+    }
+
+    public String getExtendedString() {
+        return extendedString;
+    }
+
+    public void setExtendedString(String extendedString) {
+        this.extendedString = extendedString;
+    }
+
+    public int getExtendedInt() {
+        return extendedInt;
+    }
+
+    public void setExtendedInt(int extendedInt) {
+        this.extendedInt = extendedInt;
+    }
+
+
+
+
+}
