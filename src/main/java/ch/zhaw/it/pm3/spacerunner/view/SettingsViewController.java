834483c278f2a3944b99f75cd75774226c48ec87
freymar1 <freymar1@students.zhaw.ch>
2020-10-29 17:01:03 +0100

    designed GUI, worked on settings

--- src/main/java/ch/zhaw/it/pm3/spacerunner/view/SettingsViewController.java
+++ src/main/java/ch/zhaw/it/pm3/spacerunner/view/SettingsViewController.java
@@ -1,10 +1,12 @@
 package ch.zhaw.it.pm3.spacerunner.view;
 
 import ch.zhaw.it.pm3.spacerunner.technicalservices.persistence.PersistenceUtil;
+import ch.zhaw.it.pm3.spacerunner.technicalservices.persistence.PlayerProfile;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 
@@ -12,15 +14,35 @@ public class SettingsViewController extends ViewController implements EventHandl
 
     public Button homeButton;
     public TextField playerName;
+    public Slider soundVolume;
+    public Slider framerate;
+    PlayerProfile player;
 
     @FXML
     public void showMenu() {
+        player.setPlayerName(playerName.getText());
+        player.setVolume((int) soundVolume.getValue());
+        player.setFps((int) framerate.getValue());
+        PersistenceUtil.saveProfile(player);
         getMain().setFXMLView("view/menu.fxml");
     }
 
     @Override
     public void initialize() {
-        playerName.setText(PersistenceUtil.loadProfile().getPlayerName());
+        player = PersistenceUtil.loadProfile();
+        playerName.setText(player.getPlayerName());
+        soundVolume.setValue(player.getVolume());
+        framerate.setValue(player.getFps());
+
+        playerName.textProperty().addListener((obs, oldVal, newVal) -> {
+            if (newVal.equals("")) {
+                playerName.setStyle("-fx-border-color: red");
+                homeButton.setDisable(true);
+            } else {
+                playerName.setStyle("-fx-border-color: white");
+                homeButton.setDisable(false);
+            }
+        });
 
     }
 
