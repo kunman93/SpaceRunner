dce7b26b3368d4aeadd1cded576320aa9ddbd66e
Michael Isler <islermic@students.zhaw.ch>
2020-11-02 18:13:00 +0100

    Rotate test

--- src/main/java/ch/zhaw/it/pm3/spacerunner/technicalservices/visual/VisualUtil.java
+++ src/main/java/ch/zhaw/it/pm3/spacerunner/technicalservices/visual/VisualUtil.java
@@ -162,5 +162,44 @@ public class VisualUtil {
         return image;
     }
 
+//    public BufferedImage rotateImageByDegrees(BufferedImage img, double angle) {
+//
+//        double rads = Math.toRadians(angle);
+//        double sin = Math.abs(Math.sin(rads)), cos = Math.abs(Math.cos(rads));
+//        int w = img.getWidth();
+//        int h = img.getHeight();
+//        int newWidth = (int) Math.floor(w * cos + h * sin);
+//        int newHeight = (int) Math.floor(h * cos + w * sin);
+//
+//        BufferedImage rotated = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_ARGB);
+//        Graphics2D g2d = rotated.createGraphics();
+//        AffineTransform at = new AffineTransform();
+//        at.translate((newWidth - w) / 2, (newHeight - h) / 2);
+//
+//        int x = w / 2;
+//        int y = h / 2;
+//
+//        at.rotate(rads, x, y);
+//        g2d.setTransform(at);
+//        g2d.drawImage(img, 0, 0, this);
+//        g2d.setColor(Color.RED);
+//        g2d.drawRect(0, 0, newWidth - 1, newHeight - 1);
+//        g2d.dispose();
+//
+//        return rotated;
+//    }
+
+
+   public BufferedImage rotateImage(BufferedImage bufferedImage){
+
+       AffineTransform tx = new AffineTransform();
+       tx.rotate(0.5, bufferedImage.getWidth() / 2, bufferedImage.getHeight() / 2);
+
+       AffineTransformOp op = new AffineTransformOp(tx,
+               AffineTransformOp.TYPE_BILINEAR);
+       bufferedImage = op.filter(bufferedImage, null);
+
+       return bufferedImage;
+   }
 
 }
