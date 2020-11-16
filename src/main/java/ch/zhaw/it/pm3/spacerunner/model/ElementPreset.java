8bd7c68df53283fbe40af197a614b5ceafd4786d
Peter Blattmann <blattpet@students.zhaw.ch>
2020-11-16 16:35:22 +0100

    preset get height from VisualManager

--- src/main/java/ch/zhaw/it/pm3/spacerunner/model/ElementPreset.java
+++ src/main/java/ch/zhaw/it/pm3/spacerunner/model/ElementPreset.java
@@ -5,12 +5,7 @@ import ch.zhaw.it.pm3.spacerunner.model.spaceelement.*;
 import ch.zhaw.it.pm3.spacerunner.technicalservices.visual.VisualManager;
 import ch.zhaw.it.pm3.spacerunner.technicalservices.visual.VisualNotSetException;
 
-import java.awt.*;
 import java.awt.geom.Point2D;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class ElementPreset {
     VisualManager visualManager = VisualManager.getInstance();
@@ -19,21 +14,23 @@ public class ElementPreset {
         double y;
         switch (p) {
             case ASTERIOD:
-                y = Math.random() * 1.1 - 0.3;
+                y = (Math.random() * (1.0 - 2 * visualManager.getElementRelativeHeight(Asteroid.class))) + visualManager.getElementRelativeHeight(Asteroid.class);
                 return new Preset(new SpaceElement[]{new Asteroid(new Point2D.Double(1,y))});
-            case COINS_LINE:
-                return new Preset(new SpaceElement[]{new Coin(new Point2D.Double(1, .5)), new Coin(new Point2D.Double(1.2, .5)), new Coin(new Point2D.Double(1.4, .5)), new Coin(new Point2D.Double(1.6, .5))});
+            //case COINS_LINE:
+            //    return new Preset(new SpaceElement[]{new Coin(new Point2D.Double(1, .5)), new Coin(new Point2D.Double(1.2, .5)), new Coin(new Point2D.Double(1.4, .5)), new Coin(new Point2D.Double(1.6, .5))});
             case UFOS_MIRRORED:
                 return new Preset(new SpaceElement[]{new UFO(new Point2D.Double(1,0)), new UFO(new Point2D.Double(1,0), 0.5)});
             case ROCKET:
-                y = Math.random() * (1 - visualManager.getElementRelativeWidth(Rocket.class));
+                y = (Math.random() * (1.0 - 2 * visualManager.getElementRelativeHeight(Rocket.class))) + visualManager.getElementRelativeHeight(Rocket.class);
                 return new Preset(new SpaceElement[]{new Rocket(new Point2D.Double(1,y))});
             case COINS_ARROW:
-                return new Preset(new SpaceElement[]{new Coin(new Point2D.Double(1,0.6)), new Coin(new Point2D.Double(1.05,0.7)), new Coin(new Point2D.Double(1.05,0.5)), new Coin(new Point2D.Double(1.1,0.8)),
-                        new Coin(new Point2D.Double(1.1,0.4)), new Coin(new Point2D.Double(1.1,0.6)), new Coin(new Point2D.Double(1.2,0.6)), new Coin(new Point2D.Double(1.3,0.6)),
-                        new Coin(new Point2D.Double(1.4,0.6))});
-            case COINS_RANDOM:
-                return new Preset(randomCoin());
+                y = (Math.random() * (0.7 - 0.3)) + 0.3;
+                return new Preset(new SpaceElement[]{new Coin(new Point2D.Double(1,y)), new Coin(new Point2D.Double(1.05,y+0.1)), new Coin(new Point2D.Double(1.05,y-0.1)), new Coin(new Point2D.Double(1.1,y+0.2)),
+                        new Coin(new Point2D.Double(1.1,y-0.2)), new Coin(new Point2D.Double(1.1,y)), new Coin(new Point2D.Double(1.2,y)), new Coin(new Point2D.Double(1.3,y)),
+                        new Coin(new Point2D.Double(1.4,y))});
+            case COINS_RANDOM_LINE:
+                y = (Math.random() * (1.0 - 2 * visualManager.getElementRelativeHeight(Coin.class)) + visualManager.getElementRelativeHeight(Coin.class));
+                return new Preset(randomCoinLine(y));
         }
         return null;
     }
@@ -50,13 +47,13 @@ public class ElementPreset {
         return null;
     }
 
-    private SpaceElement[] randomCoin() {
-        double position_y = (Math.random() * (0.9 - 0.1)) + 0.1;
-        double position_x = 1.0;
-        SpaceElement[] spaceElements = new SpaceElement[5];
-        for (int i = 1; i <= 5; i++) {
-            spaceElements[i-1] = new Coin(new Point2D.Double(position_x,position_y));
-            position_x = position_x + 0.1;
+    private SpaceElement[] randomCoinLine(double y) {
+        double x = 1.0;
+        int count = (int) (Math.random() * 10);
+        SpaceElement[] spaceElements = new SpaceElement[count];
+        for (int i = 1; i <= count; i++) {
+            spaceElements[i-1] = new Coin(new Point2D.Double(x,y));
+            x = x + 0.1;
         }
         return spaceElements;
     }
--- src/main/java/ch/zhaw/it/pm3/spacerunner/model/spaceelement/PresetType.java
+++ src/main/java/ch/zhaw/it/pm3/spacerunner/model/spaceelement/PresetType.java
@@ -2,9 +2,9 @@ package ch.zhaw.it.pm3.spacerunner.model.spaceelement;
 
 public enum PresetType {
     ASTERIOD,
-    COINS_LINE,
+    //COINS_LINE,
     UFOS_MIRRORED,
     ROCKET,
     COINS_ARROW,
-    COINS_RANDOM
+    COINS_RANDOM_LINE
 }
