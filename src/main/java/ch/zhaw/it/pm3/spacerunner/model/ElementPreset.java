43301088bff9d924557c3f92263af6fbe376abad
hiddenmind3 <hirschi.evan@gmail.com>
2020-11-16 15:20:13 +0100

    created PresetType Enum

--- src/main/java/ch/zhaw/it/pm3/spacerunner/model/ElementPreset.java
+++ src/main/java/ch/zhaw/it/pm3/spacerunner/model/ElementPreset.java
@@ -15,26 +15,28 @@ import java.util.TimerTask;
 public class ElementPreset {
     VisualManager visualManager = VisualManager.getInstance();
 
-    private Preset generatePreset(int index) throws VisualNotSetException {
-        switch (index) {
-            case 0:
-                return new Preset(new SpaceElement[]{new Asteroid(new Point2D.Double(1,0))});
-            case 1:
+    private Preset generatePreset(PresetType p) throws VisualNotSetException {
+        double y;
+        switch (p) {
+            case ASTERIOD:
+                y = Math.random() * 1.1 - 0.3;
+                return new Preset(new SpaceElement[]{new Asteroid(new Point2D.Double(1,y))});
+            case COINS_LINE:
                 return new Preset(new SpaceElement[]{new Coin(new Point2D.Double(1, .5)), new Coin(new Point2D.Double(1.2, .5)), new Coin(new Point2D.Double(1.4, .5)), new Coin(new Point2D.Double(1.6, .5))});
-            case 2:
+            case UFOS_MIRRORED:
                 return new Preset(new SpaceElement[]{new UFO(new Point2D.Double(1,0)), new UFO(new Point2D.Double(1,0), 0.5)});
-            case 3:
-                double y = Math.random() * (1 - visualManager.getElementRelativeWidth(Rocket.class));
+            case ROCKET:
+                y = Math.random() * (1 - visualManager.getElementRelativeWidth(Rocket.class));
                 return new Preset(new SpaceElement[]{new Rocket(new Point2D.Double(1,y))});
         }
         return null;
     }
 
     public synchronized Preset getRandomPreset() {
-        int index = (int)Math.floor(Math.random() * 4);
+        int index = (int)Math.floor(Math.random() * (PresetType.values().length));
 
         try {
-            return generatePreset(index);
+            return generatePreset(PresetType.values()[index]);
         } catch (VisualNotSetException e) {
             e.printStackTrace();
         }
---
+++ src/main/java/ch/zhaw/it/pm3/spacerunner/model/spaceelement/PresetType.java
@@ -0,0 +1,8 @@
+package ch.zhaw.it.pm3.spacerunner.model.spaceelement;
+
+public enum PresetType {
+    ASTERIOD,
+    COINS_LINE,
+    UFOS_MIRRORED,
+    ROCKET
+}
