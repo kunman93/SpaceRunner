dc70d670ecfb70772ea413e9e5810cd91cb95250
Manu Kunnumpurathu <kunnuman@students.zhaw.ch>
2020-11-16 17:39:01 +0100

    imported class

--- src/main/java/ch/zhaw/it/pm3/spacerunner/view/ShopViewController.java
+++ src/main/java/ch/zhaw/it/pm3/spacerunner/view/ShopViewController.java
@@ -6,6 +6,7 @@ import ch.zhaw.it.pm3.spacerunner.technicalservices.persistence.ShopContent;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TabPane;
 
