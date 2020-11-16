72c2561dcbbddf96a63623e8c876051ba780c64e
Peter Blattmann <blattpet@students.zhaw.ch>
2020-11-16 15:21:19 +0100

    preset variable coin line

--- src/main/java/ch/zhaw/it/pm3/spacerunner/model/ElementPreset.java
+++ src/main/java/ch/zhaw/it/pm3/spacerunner/model/ElementPreset.java
@@ -21,7 +21,8 @@ public class ElementPreset {
                 new Preset(new SpaceElement[]{new Rocket(new Point2D.Double(1,0.2))}),
                 new Preset(new SpaceElement[]{new Coin(new Point2D.Double(1,0.6)), new Coin(new Point2D.Double(1.05,0.7)), new Coin(new Point2D.Double(1.05,0.5)), new Coin(new Point2D.Double(1.1,0.8)),
                         new Coin(new Point2D.Double(1.1,0.4)), new Coin(new Point2D.Double(1.1,0.6)), new Coin(new Point2D.Double(1.2,0.6)), new Coin(new Point2D.Double(1.3,0.6)),
-                        new Coin(new Point2D.Double(1.4,0.6))}) //arrow
+                        new Coin(new Point2D.Double(1.4,0.6))}), //arrow
+                new Preset(randomCoin())
         };
     }
 
@@ -32,5 +33,16 @@ public class ElementPreset {
     public synchronized Preset getRandomPreset() {
         int index = (int)Math.floor(Math.random() * presets.length);
         return presets[index];
+    }
+
+    private SpaceElement[] randomCoin() {
+        double position_y = (Math.random() * (0.9 - 0.1)) + 0.1;
+        double position_x = 1.0;
+        SpaceElement[] spaceElements = new SpaceElement[5];
+        for (int i = 1; i <= 5; i++) {
+            spaceElements[i-1] = new Coin(new Point2D.Double(position_x,position_y));
+            position_x = position_x + 0.1;
+        }
+        return spaceElements;
     }
 }
