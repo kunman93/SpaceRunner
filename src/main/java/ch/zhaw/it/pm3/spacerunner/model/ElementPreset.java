51446f3e493fd80e3d3ea4e557f7f55e2d58c0f1
Peter Blattmann <blattpet@students.zhaw.ch>
2020-11-16 14:32:05 +0100

    preset new arrow

--- src/main/java/ch/zhaw/it/pm3/spacerunner/model/ElementPreset.java
+++ src/main/java/ch/zhaw/it/pm3/spacerunner/model/ElementPreset.java
@@ -4,13 +4,11 @@ package ch.zhaw.it.pm3.spacerunner.model;
 import ch.zhaw.it.pm3.spacerunner.model.spaceelement.*;
 import ch.zhaw.it.pm3.spacerunner.technicalservices.visual.VisualManager;
 import ch.zhaw.it.pm3.spacerunner.technicalservices.visual.VisualNotSetException;
+import org.checkerframework.checker.units.qual.C;
 
 import java.awt.*;
 import java.awt.geom.Point2D;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.lang.Math;
 
 public class ElementPreset {
     private Preset[] presets = generatePresets();
@@ -20,7 +18,10 @@ public class ElementPreset {
                 new Preset(new SpaceElement[]{new Asteroid(new Point2D.Double(1,0))}),
                 new Preset(new SpaceElement[]{new Coin(new Point2D.Double(1, .5)), new Coin(new Point2D.Double(1.2, .5)), new Coin(new Point2D.Double(1.4, .5)), new Coin(new Point2D.Double(1.6, .5))}),
                 new Preset(new SpaceElement[]{new UFO(new Point2D.Double(1,0)), new UFO(new Point2D.Double(1,0), 0.5)}),
-                new Preset(new SpaceElement[]{new Rocket(new Point2D.Double(1,0.2))})
+                new Preset(new SpaceElement[]{new Rocket(new Point2D.Double(1,0.2))}),
+                new Preset(new SpaceElement[]{new Coin(new Point2D.Double(1,0.6)), new Coin(new Point2D.Double(1.05,0.7)), new Coin(new Point2D.Double(1.05,0.5)), new Coin(new Point2D.Double(1.1,0.8)),
+                        new Coin(new Point2D.Double(1.1,0.4)), new Coin(new Point2D.Double(1.1,0.6)), new Coin(new Point2D.Double(1.2,0.6)), new Coin(new Point2D.Double(1.3,0.6)),
+                        new Coin(new Point2D.Double(1.4,0.6))}) //arrow
         };
     }
 
