197fc5fdae87989d2913b784bf39a91b8068963d
freymar1 <freymar1@students.zhaw.ch>
2020-11-18 23:02:12 +0100

    added icons to infobar

--- src/main/java/ch/zhaw/it/pm3/spacerunner/technicalservices/visual/manager/UIVisualElement.java
+++ src/main/java/ch/zhaw/it/pm3/spacerunner/technicalservices/visual/manager/UIVisualElement.java
@@ -3,5 +3,7 @@ package ch.zhaw.it.pm3.spacerunner.technicalservices.visual.manager;
 import ch.zhaw.it.pm3.spacerunner.technicalservices.visual.manager.VisualElement;
 
 public enum UIVisualElement implements VisualElement {
-    COIN_COUNT;
+    COIN_COUNT,
+    DOUBLE_COIN_POWER_UP,
+    SHIELD_POWER_UP;
 }
