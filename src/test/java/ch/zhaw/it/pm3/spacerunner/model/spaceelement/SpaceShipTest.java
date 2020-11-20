4cc472774b2389722d11c33834d76d8ef2af3e44
hiddenmind3 <hirschi.evan@gmail.com>
2020-11-20 08:58:47 +0100

    created Tests for SpaceShipTest.java; Removed CoinTest.java

--- src/test/java/ch/zhaw/it/pm3/spacerunner/model/spaceelement/SpaceShipTest.java
+++ src/test/java/ch/zhaw/it/pm3/spacerunner/model/spaceelement/SpaceShipTest.java
@@ -1,42 +1,90 @@
 package ch.zhaw.it.pm3.spacerunner.model.spaceelement;
 import ch.zhaw.it.pm3.spacerunner.model.spaceelement.velocity.VelocityManager;
-import ch.zhaw.it.pm3.spacerunner.model.spaceelement.speed.HorizontalSpeed;
-import ch.zhaw.it.pm3.spacerunner.model.spaceelement.speed.VerticalSpeed;
+import ch.zhaw.it.pm3.spacerunner.model.spaceelement.velocity.VelocityNotSetException;
+import ch.zhaw.it.pm3.spacerunner.technicalservices.visual.manager.Visual;
+import ch.zhaw.it.pm3.spacerunner.technicalservices.visual.manager.VisualManager;
+import ch.zhaw.it.pm3.spacerunner.technicalservices.visual.manager.VisualNotSetException;
+import ch.zhaw.it.pm3.spacerunner.technicalservices.visual.manager.VisualScaling;
+import ch.zhaw.it.pm3.spacerunner.technicalservices.visual.util.VisualSVGFile;
 import org.junit.jupiter.api.*;
 
-import java.awt.*;
 import java.awt.geom.Point2D;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SpaceShipTest {
-    private SpaceShip ship;
+    private SpaceShip spaceShip;
     private VelocityManager velocityManager = VelocityManager.getManager();
+    private VisualManager visualManager = VisualManager.getManager();
 
-    /*@BeforeEach
+    @BeforeEach
     void setUp() {
-        ship = new SpaceShip(new Point(0,0));
-        velocityManager.setWidth(100);
-        velocityManager.setHeight(100);
-        velocityManager.setVelocity(SpaceShip.class, new Point2D.Double(HorizontalSpeed.ZERO.getSpeed(), VerticalSpeed.SPACE_SHIP.getSpeed()));
+        velocityManager.setupGameElementVelocity();
+        visualManager.loadAndSetVisual(SpaceShip.class, new Visual(VisualSVGFile.SPACE_SHIP_1, VisualScaling.SPACE_SHIP));
     }
 
     @Test
-    void directMoveUpTest(){
-        assertEquals(new Point(0,0), ship.getRelativePosition());
-        ship.directMoveUp();
-        assertEquals(new Point(0,-2), ship.getRelativePosition());
-        ship.directMoveUp();
-        assertEquals(new Point(0,-4), ship.getRelativePosition());
+    void directMoveUp1SecTest() {
+        spaceShip = new SpaceShip(new Point2D.Double(0,1));
+        spaceShip.moveSpaceShip(SpaceShipDirection.UP, 1000);
+        try {
+            assertEquals(spaceShip.getRelativePosition(), new Point2D.Double(0, 1 - velocityManager.getRelativeVelocity(SpaceShip.class).y));
+        } catch (VelocityNotSetException exception) {
+            fail("VelocityNotSetException thrown");
+        }
     }
 
     @Test
-    void directMoveDownTest(){
-        assertEquals(new Point(0,0), ship.getRelativePosition());
-        ship.directMoveDown();
-        assertEquals(new Point(0,2), ship.getRelativePosition());
-        ship.directMoveDown();
-        assertEquals(new Point(0,4), ship.getRelativePosition());
-    }*/
+    void directMoveUp1MillisTest() {
+        spaceShip = new SpaceShip(new Point2D.Double(0,1));
+        spaceShip.moveSpaceShip(SpaceShipDirection.UP, 1);
+        try {
+            assertEquals(spaceShip.getRelativePosition(), new Point2D.Double(0, 1 - (1.0/1000.0)*velocityManager.getRelativeVelocity(SpaceShip.class).y));
+        } catch (VelocityNotSetException exception) {
+            fail("VelocityNotSetException thrown");
+        }
+    }
 
+    @Test
+    void directMoveDown1SecTest() {
+        spaceShip = new SpaceShip(new Point2D.Double(0,0));
+        spaceShip.moveSpaceShip(SpaceShipDirection.DOWN, 1000);
+        try {
+            assertEquals(spaceShip.getRelativePosition(), new Point2D.Double(0, 0 + velocityManager.getRelativeVelocity(SpaceShip.class).y));
+        } catch (VelocityNotSetException exception) {
+            fail("VelocityNotSetException thrown");
+        }
+    }
+
+    @Test
+    void directMoveDown1MillisTest() {
+        spaceShip = new SpaceShip(new Point2D.Double(0,0));
+        spaceShip.moveSpaceShip(SpaceShipDirection.DOWN, 1);
+        try {
+            assertEquals(spaceShip.getRelativePosition(), new Point2D.Double(0, 0 + (1.0/1000.0)*velocityManager.getRelativeVelocity(SpaceShip.class).y));
+        } catch (VelocityNotSetException exception) {
+            fail("VelocityNotSetException thrown");
+        }
+    }
+
+    @Test
+    void directMoveDownPastScreenLimitTest() {
+        spaceShip = new SpaceShip(new Point2D.Double(0,0));
+        spaceShip.moveSpaceShip(SpaceShipDirection.DOWN, 1000);
+        spaceShip.moveSpaceShip(SpaceShipDirection.DOWN, 1000);
+        try {
+            assertEquals(spaceShip.getRelativePosition(), new Point2D.Double(0, 1 - visualManager.getElementRelativeHeight(SpaceShip.class)));
+        } catch (VisualNotSetException e) {
+            fail("VisualNotSetException thrown");
+        }
+    }
+
+    @Test
+    void directMoveUpPastScreenLimitTest() {
+        spaceShip = new SpaceShip(new Point2D.Double(0,0.5));
+        spaceShip.moveSpaceShip(SpaceShipDirection.UP, 1000);
+        spaceShip.moveSpaceShip(SpaceShipDirection.UP, 1000);
+        assertEquals(spaceShip.getRelativePosition(), new Point2D.Double(0, 0));
+    }
 }
