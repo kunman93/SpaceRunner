28526ed564b042e0546f6a26f67ff81b0d094725
Manu Kunnumpurathu <kunnuman@students.zhaw.ch>
2020-11-22 14:04:03 +0100

    added author

--- src/main/java/ch/zhaw/it/pm3/spacerunner/ui/ShopContentCellControllerListener.java
+++ src/main/java/ch/zhaw/it/pm3/spacerunner/ui/ShopContentCellControllerListener.java
@@ -3,8 +3,10 @@ package ch.zhaw.it.pm3.spacerunner.ui;
 /**
  * The ShopContentCellControllerListener is used to depict the updated coin-value in ShopViewController,
  * when a shop content was purchased in ShopContentCellController by pressing the buy-Button.
+ * @author kunnuman
  */
 public interface ShopContentCellControllerListener {
+    
     /**
      * Performs the action when a shop content was purchased.
      */
